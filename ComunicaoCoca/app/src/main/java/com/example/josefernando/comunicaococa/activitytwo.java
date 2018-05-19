ab543d117a3154acc5f7a90c40ccde2dbe6fc009
Rafael Rosenzvaig <rafaelvr@al.insper.edu.br>
2018-05-19 20:58:26 -0300

    fix1 teste cards Eventos

--- ComunicaoCoca/app/src/main/java/com/example/josefernando/comunicaococa/activitytwo.java
+++ ComunicaoCoca/app/src/main/java/com/example/josefernando/comunicaococa/activitytwo.java
@@ -3,14 +3,40 @@ package com.example.josefernando.comunicaococa;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class activitytwo extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] dataToTest = new String[10];
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
+        mRecyclerView = (RecyclerView) findViewById(R.id.reciclerView1);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(dataToTest);
+        mRecyclerView.setAdapter(mAdapter);
+
+        dataToTest[0] = "Mac";
+        dataToTest[1] = "and";
+        dataToTest[2] = "cheese";
     }
 
     public void mainactivity (View v) {
@@ -19,5 +45,5 @@ public class activitytwo extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-
 }
+
