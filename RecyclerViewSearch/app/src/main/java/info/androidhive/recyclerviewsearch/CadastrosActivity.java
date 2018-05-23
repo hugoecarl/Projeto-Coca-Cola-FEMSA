d169a27fcaab73e5803bf95c4147d5b87d76ae3d
Rafael Rosenzvaig <rafaelvr@al.insper.edu.br>
2018-05-23 16:01:47 -0300

    Cards Tests 1

---
+++ RecyclerViewSearch/app/src/main/java/info/androidhive/recyclerviewsearch/CadastrosActivity.java
@@ -0,0 +1,131 @@
+package info.androidhive.recyclerviewsearch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+
+import info.androidhive.recyclerviewsearch.DAO.ConfiguracaoFirebase;
+import info.androidhive.recyclerviewsearch.Entidades.Usuarios;
+import info.androidhive.recyclerviewsearch.Helper.Base64Custom;
+import info.androidhive.recyclerviewsearch.Helper.Preferencias;
+
+public class CadastrosActivity extends AppCompatActivity {
+
+    private EditText edtCadEmail;
+    private EditText edtCadNome;
+    private EditText edtCadSobrenome;
+    private EditText edtCadSenha;
+    private EditText edtCadConfirmaSenha;
+    private EditText edtCadNascimento;
+    private RadioButton rbMasculino;
+    private RadioButton rbFeminino;
+    private Button btnGravar;
+    private Usuarios usuarios;
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastros);
+
+        edtCadEmail = (EditText) findViewById(R.id.edtCadEmail);
+        edtCadNome = (EditText) findViewById(R.id.edtCadNome);
+        edtCadSobrenome = (EditText) findViewById(R.id.edtCadSobrenome);
+        edtCadSenha = (EditText) findViewById(R.id.edtCadSenha);
+        edtCadConfirmaSenha = (EditText) findViewById(R.id.edtCadConfirmaSenha);
+        edtCadNascimento = (EditText) findViewById(R.id.edtCadNascimento);
+        rbFeminino = (RadioButton) findViewById(R.id.rbFeminino);
+        rbMasculino = (RadioButton) findViewById(R.id.rbMasculino);
+        btnGravar = (Button) findViewById(R.id.btnGravar);
+
+        btnGravar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtCadSenha.getText().toString().equals(edtCadConfirmaSenha.getText().toString())){
+                    usuarios = new Usuarios();
+                    usuarios.setNome(edtCadNome.getText().toString());
+                    usuarios.setEmail(edtCadEmail.getText().toString());
+                    usuarios.setSenha(edtCadSenha.getText().toString());
+                    usuarios.setNascimento(edtCadNascimento.getText().toString());
+                    usuarios.setSobrenome(edtCadSobrenome.getText().toString());
+
+                    if (rbFeminino.isChecked()){
+                        usuarios.setSexo("Feminino");
+                    } else {
+                        usuarios.setSexo("Masculino");
+                    }
+                    cadastrarUsuario();
+                }else {
+                    Toast.makeText(CadastrosActivity.this,"As senhas não são correspondentes", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        }
+
+    private void cadastrarUsuario(){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuarios.getEmail()+"@coca.com",
+                usuarios.getSenha()
+        ).addOnCompleteListener(CadastrosActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(CadastrosActivity.this,"Usuario cadastrado com sucesso", Toast.LENGTH_LONG).show();
+
+                    String identificadorUsuario = Base64Custom.codificarBase64(usuarios.getEmail());
+
+                    FirebaseUser usuarioFirebase = task.getResult().getUser();
+                    usuarios.setId(identificadorUsuario);
+                    usuarios.salvar();
+
+                    Preferencias preferenciasAndroid = new Preferencias(CadastrosActivity.this);
+                    preferenciasAndroid.salvarUsuarioPreferencias(identificadorUsuario, usuarios.getNome());
+                    abrirLoginUsuario();
+                }else {
+                    String erroExcecao = "";
+
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        erroExcecao = "Digite uma senha mais forte, contendo no mínimo 8 caracteres de letras e números";
+                    }catch (FirebaseAuthInvalidCredentialsException e) {
+                        erroExcecao = "O e-mail digitado é invalido, digite um novo e-mail";
+                    }
+                    catch (FirebaseAuthUserCollisionException e) {
+                        erroExcecao = "O ID digitado já está cadastrado no sistema";
+                    }
+                    catch (Exception e) {
+                        erroExcecao = "Erro ao efetuar o cadastro";
+                    }
+                    Toast.makeText(CadastrosActivity.this,"Erro: " + erroExcecao, Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        }
+    public void abrirLoginUsuario(){
+        Intent intent = new Intent(CadastrosActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
---
+++ RecyclerViewSearch/app/src/main/java/info/androidhive/recyclerviewsearch/DAO/ConfiguracaoFirebase.java
@@ -0,0 +1,27 @@
+package info.androidhive.recyclerviewsearch.DAO;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ConfiguracaoFirebase {
+
+     private  static DatabaseReference referenciaFirebase;
+     private  static FirebaseAuth autenticacao;
+
+
+     public static DatabaseReference getFirebase() {
+          if (referenciaFirebase == null){
+              referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+          }
+          return referenciaFirebase;
+     }
+
+     public static  FirebaseAuth getFirebaseAutenticacao(){
+          if (autenticacao == null){
+              autenticacao = FirebaseAuth.getInstance();
+          }
+          return autenticacao;
+     }
+
+}
